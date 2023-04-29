79660d2ea141520e920657c1c60978d830a52274
amirtharajanpks <amirtha.sureshkumar@nagarro.com>
2023-04-29 12:58:33 +0530

    React my-app session 51

--- UI/React/Udemy/my-app/src/App.js
+++ UI/React/Udemy/my-app/src/App.js
@@ -1,7 +1,10 @@
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+const App = () => {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
+      <Expenses/>
     </div>
   );
 }
