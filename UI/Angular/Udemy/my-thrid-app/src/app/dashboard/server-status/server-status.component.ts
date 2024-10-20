ca43264fbac6994ce45082619ad50b79513dd417
amirtharajanpks <amirtharajanpks@hotmail.com>
2024-10-20 15:26:10 +0530

    viewChild and formElement

--- UI/Angular/Udemy/my-thrid-app/src/app/dashboard/server-status/server-status.component.ts
+++ UI/Angular/Udemy/my-thrid-app/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, ElementRef, inject, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnDestroy {
+  currentStatus : 'online' | 'offline' | 'unknown' = 'online';
+  private interval? : ReturnType<typeof setInterval>;
+  private destroy = inject(DestroyRef);
+  private el = inject(ElementRef);
+
+  constructor() {  }
+
+  ngOnInit() {
+    this.interval =setInterval(()=>{
+      const random = Math.random();
+      if(random < 0.5) {
+        this.currentStatus = 'online';
+      }
+      else if (random < 0.9) {
+        this.currentStatus = 'offline';
+      }
+      else {
+        this.currentStatus = 'unknown';
+      }
+    },5000)
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
 }
